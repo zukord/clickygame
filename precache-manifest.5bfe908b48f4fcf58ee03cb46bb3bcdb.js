8dfba8652582bbcfc3c368b89c1daafcf8643247
David Zukor <zukord@gmail.com>
2019-06-12 02:38:52 -0400

    Updates

---
+++ precache-manifest.5bfe908b48f4fcf58ee03cb46bb3bcdb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb213c0214de73a40e4f5b3c392e365a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2763bab6a4e9a8582a8e",
+    "url": "/static/css/main.c72fdd41.chunk.css"
+  },
+  {
+    "revision": "8a60076f748627df30d1",
+    "url": "/static/js/2.d7a4257c.chunk.js"
+  },
+  {
+    "revision": "2763bab6a4e9a8582a8e",
+    "url": "/static/js/main.f69e06fa.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7e6be2a9a5ecfd394bcc6a14087fe0ca",
+    "url": "/static/media/brown.7e6be2a9.jpg"
+  }
+]);
